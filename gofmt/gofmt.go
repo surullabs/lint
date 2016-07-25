09a70ae7917c0fee6c4209aa0fd13dc5b0af7333
sridharv <sridhar@laddoo.net>
2016-07-25 12:50:53 +0200

    Use gofmt directly instead of go fmt

--- gofmt/gofmt.go
+++ gofmt/gofmt.go
@@ -4,13 +4,18 @@ import (
 	"bytes"
 	"fmt"
 	"os/exec"
+	"github.com/surullabs/statictest"
 )
 
 type Check struct {
 }
 
 func (Check) Check(pkg string) error {
-	data, err := exec.Command("go", "fmt", pkg).CombinedOutput()
+	files, err := statictest.GoFiles(pkg)
+	if err != nil {
+		return err
+	}
+	data, err := exec.Command("gofmt", append([]string{"-d"}, files...)...).CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("%v: %s", err, string(data))
 	}
