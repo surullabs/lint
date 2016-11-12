bd01dc93c8ad8df998e1e2f2475cbfb5b19fbf09
sridharv <sridhar@laddoo.net>
2016-11-12 21:53:25 +0100

    Collect coverage for checkers as well

--- testcovered.sh
+++ testcovered.sh
@@ -5,4 +5,6 @@ set -x
 
 go list -f '{{if len .XTestGoFiles}}"go test -coverprofile={{.Dir}}/.coverprofile {{.ImportPath}}"
 {{end}}' ./... | xargs -L 1 sh -c
+rm ./.coverprofile
+go test -v -coverprofile=./.coverprofile -coverpkg github.com/surullabs/lint,github.com/surullabs/lint/checkers
 gover
